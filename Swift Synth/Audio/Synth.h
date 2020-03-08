0b0371013ee0c77526d7e898270786557f85accd
Magnus Woodgate <mwoodgate.dev@gmail.com>
2020-03-08 19:43:20 +0100

    Created test setup and empty objc class.

---
+++ Swift Synth/Audio/Synth.h
@@ -0,0 +1,17 @@
+//
+//  NSObject+Synth.h
+//  Swift Synth
+//
+//  Created by Magnus Woodgate on 08/03/2020.
+//  Copyright © 2020 Grant Emerson. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Synth : NSObject
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Swift Synth/Audio/Synth.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+Synth.m
+//  Swift Synth
+//
+//  Created by Magnus Woodgate on 08/03/2020.
+//  Copyright © 2020 Grant Emerson. All rights reserved.
+//
+
+#import "Synth.h"
+
+@implementation Synth : NSObject
+
+@end
