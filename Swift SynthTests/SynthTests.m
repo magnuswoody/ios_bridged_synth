c7de58d03e36758df2674ce1fcd6fd99212ab617
Magnus Woodgate <mwoodgate.dev@gmail.com>
2020-03-08 19:45:10 +0100

    Added test for synth shared instance instantiation.

--- Swift SynthTests/SynthTests.m
+++ Swift SynthTests/SynthTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Synth.h"
 
 @interface SynthTests : XCTestCase
 
@@ -22,16 +23,10 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testSharedInstanceIsInstantiated {
+    Synth* ref = [Synth shared];
+    XCTAssertNotNil(ref);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
